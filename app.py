d5eb4fb85d27f4fe216090d4da980f9fc7fba4e1
Himanshu <98688547+00Himanshu@users.noreply.github.com>
2024-05-02 10:39:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model=joblib.load("news-classification-model.pkl")
 news_labels={"0": "Technical", "1": "Business", "2" : "Sports", "3" : "Entertainment", "4" : "Politics"}
 
 
-st.title("News classificateion")
+st.title("News classification")
 
 #Input text area
 user_input= st.text_area("Enter news here:")
