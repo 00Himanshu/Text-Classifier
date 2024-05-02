04e707922da58c2fa577db4b65853ab653dfb403
00Himanshu <00Himanshu@users.noreply.github.com>
2024-05-02 10:30:22 +0530

    text-classifier

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import joblib
+
+#Load the trained model 
+model=joblib.load("news-classification-model.pkl")
+
+#Define sentiment labels
+news_labels={"0": "Technical", "1": "Business", "2" : "Sports", "3" : "Entertainment", "4" : "Politics"}
+
+
+st.title("News classificateion")
+
+#Input text area
+user_input= st.text_area("Enter news here:")
+
+#Pre
+if st.button("Predict"):
+    
+    predicted_label=model.predict([user_input])[0]
+
+    predicted_news_label=news_labels[str(predicted_label)]
+
+
+    st.info(f"Predicted Sentiment: {predicted_news_label}")
